fb0b64d29baef04688ab8f9ac33eb61164c70502
David Rodriguez <davidrodriguez212@gmail.com>
2013-08-11 12:08:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RegexChallenge::Application.config.secret_token = 'c824e25a0a02aa8c891482a13e4821d378f79af541e454d9bbdf38773953d7b5b1253a99ba6b188144f75b62a30bf3fcd1dc24f906523d5e95594c7f36fabf4c'
